447c71cd2a24a65c38ff30011c8a9cf07d84b00f
VladyslavGapyak <87811072+VladyslavGapyak@users.noreply.github.com>
2025-02-28 09:46:00 +0000

    Deploying to gh-pages from @ VladyslavGapyak/vladyslavgapyak.github.io@6c1e50f55f43ca5963982a1d455d1b8b7825db68 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Here is an overview of my experience as a teacher during my PhD.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
